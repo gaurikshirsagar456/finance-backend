e41d844ac3ebce6f80bcab5bdca5f13876920f99
Gauri Kshirsagar <165363408+gaurikshirsagar456@users.noreply.github.com>
2026-04-05 20:22:54 +0530

    Create auth.middleware.d.ts

---
+++ dist/src/middlewares/auth.middleware.d.ts
@@ -0,0 +1,4 @@
+import { Response, NextFunction } from 'express';
+import { AuthRequest } from '../types';
+export declare const authenticate: (req: AuthRequest, _res: Response, next: NextFunction) => void;
+//# sourceMappingURL=auth.middleware.d.ts.map
